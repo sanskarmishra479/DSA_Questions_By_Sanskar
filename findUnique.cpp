be0765e7fb39409ce32ad230600c13df0ed2437b
Sanskar Mishra <sanskarmishra479@gmail.com>
2024-01-01 22:30:36 +0530

    adding  answer

--- findUnique.cpp
+++ findUnique.cpp
@@ -1,17 +1,16 @@
 #include<iostream>
 using namespace std;
-int find(int arr[], int size){
+void find(int arr[], int size){
     int ans = 0;
     for(int i=0; i<size; i++){
         ans=ans^arr[i];
     }
-    return ans;
+    cout<<"Unique element is: "<< ans << endl;
 }
 int main(){
 
     int arr[5] = {3,4,3,4,1};
-    int c = find(arr,5);
-    cout<<"Unique element is: "<<c<<endl;
+    find(arr,5);
 
     return 0;
 }
