59668d083bc2d9e715b4a452562e8fb970133872
Sanskar Mishra <sanskarmishra479@gmail.com>
2024-01-12 23:06:11 +0530

    Adding Answer

---
+++ PivotEle.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int FindPivot(int arr[], int size){
+    int start=0,end = size-1;
+    int mid = start+(end-start)/2;
+    while(start<end){
+        if(arr[mid]>=arr[0]){
+            start = mid+1;
+        }
+        else{
+            end=mid;
+        }
+        mid = start+(end-start)/2;
+    }
+    return start;
+}
+int main(){
+
+    int arr[10]  = { 10, 9, 8, 1, 2, 3, 4, 5, 6, 7};
+    int PivotIndex = FindPivot(arr, 10);
+
+    cout<<"Pivot element is: "<<PivotIndex<<endl;
+
+    return 0;
+}
