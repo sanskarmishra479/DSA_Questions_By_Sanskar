e4f0e83057d00b6c4887f942154144dfd1f973bb
Sanskar Mishra <sanskarmishra479@gmail.com>
2024-01-12 23:47:55 +0530

    Change the Pivot function name

--- PivotEle.cpp
+++ PivotEle.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int FindPivot(int arr[], int size){
+int Pivot(int arr[], int size){
     int start=0,end = size-1;
     int mid = start+(end-start)/2;
     while(start<end){
@@ -17,7 +17,7 @@ int FindPivot(int arr[], int size){
 int main(){
 
     int arr[10]  = { 10, 9, 8, 1, 2, 3, 4, 5, 6, 7};
-    int PivotIndex = FindPivot(arr, 10);
+    int PivotIndex = Pivot(arr, 10);
 
     cout<<"Pivot element is: "<<PivotIndex<<endl;
 
